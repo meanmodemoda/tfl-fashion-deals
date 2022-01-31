dcee19a1b1445be944e12a27565f5737689d6a56
Yang Zhao <42701448+muonius@users.noreply.github.com>
2022-01-30 20:10:00 -0500

    update

--- app.js
+++ app.js
@@ -1,80 +1,17 @@
-import titleBlocks from './component.js'
+// var fs = require('fs');
+
+import dealBlock from './component.js'
 
 
 const app = Vue.createApp({
     components: {
-        titleBlocks
+        dealBlock
+        //referencing component object dealBlock not the name property of the object
     },
     data:
         function () {
             return {
-                deals: [
-                    {
-                        id: '111',
-                        title: 'G-III to Acquire Sonia Rykiel',
-                        dt: '1632787200000',
-                        updatedAt: 'Tue Sep 28 2021',
-                        country: 'United States',
-                        region: 'North America',
-                        industry: 'apparel',
-                        Year: '2021',
-                        content:
-                            'G-III Apparel Group revealed that it has entered into an agreement to purchase Sonia Rykiel, with plans to accelerate the relaunch of the French fashion brand primarily in Europe, for the fall of 2022, with collections across multiple categories. The transaction, which comes less than two years after brothers Eric and Michael Dayan successfully bid to acquire all of the bankrupt brand’s assets via a court-administered process. (Those assets included the brand’s intellectual property rights (namely, its various global trademark registrations, and decades of archives and product prototypes); the commercial leases for its brick-and-mortar outposts in France – from its Saint Germain flagship to a glitzy boutique in Cannes, among others; and its remaining stock of garments and accessories.)The deal is expected to close by the end of October 2021.'
-                    },
-
-                    {
-                        id: '123',
-                        title: 'G-III to Acquire Sonia Rykiel',
-                        dt: '1632787200000',
-                        updatedAt: 'Tue Sep 28 2021',
-                        country: 'United States',
-                        region: 'North America',
-                        industry: 'footwear',
-                        Year: '2021',
-                        content:
-                            'G-III Apparel Group revealed that it has entered into an agreement to purchase Sonia Rykiel, with plans to accelerate the relaunch of the French fashion brand primarily in Europe, for the fall of 2022, with collections across multiple categories. The transaction, which comes less than two years after brothers Eric and Michael Dayan successfully bid to acquire all of the bankrupt brand’s assets via a court-administered process. (Those assets included the brand’s intellectual property rights (namely, its various global trademark registrations, and decades of archives and product prototypes); the commercial leases for its brick-and-mortar outposts in France – from its Saint Germain flagship to a glitzy boutique in Cannes, among others; and its remaining stock of garments and accessories.)The deal is expected to close by the end of October 2021.'
-                    },
-
-
-                    {
-                        id: '233',
-                        title: 'ABCFEF',
-                        dt: '1632787200000',
-                        updatedAt: 'Tue Sep 28 2021',
-                        country: 'United States',
-                        region: 'North America',
-                        industry: 'footwear',
-                        Year: '2021',
-                        content:
-                            'G-III Apparel Group revealed that it has entered into an agreement to purchase Sonia Rykiel, with plans to accelerate the relaunch of the French fashion brand primarily in Europe, for the fall of 2022, with collections across multiple categories. The transaction, which comes less than two years after brothers Eric and Michael Dayan successfully bid to acquire all of the bankrupt brand’s assets via a court-administered process. (Those assets included the brand’s intellectual property rights (namely, its various global trademark registrations, and decades of archives and product prototypes); the commercial leases for its brick-and-mortar outposts in France – from its Saint Germain flagship to a glitzy boutique in Cannes, among others; and its remaining stock of garments and accessories.)The deal is expected to close by the end of October 2021.'
-                    },
-
-                    {
-                        id: '345',
-                        title: 'Happy Go Lucky',
-                        dt: '1632787200000',
-                        updatedAt: 'Tue Sep 28 2021',
-                        country: 'United States',
-                        region: 'North America',
-                        industry: 'footwear',
-                        Year: '2021',
-                        content:
-                            'G-III Apparel Group revealed that it has entered into an agreement to purchase Sonia Rykiel, with plans to accelerate the relaunch of the French fashion brand primarily in Europe, for the fall of 2022, with collections across multiple categories. The transaction, which comes less than two years after brothers Eric and Michael Dayan successfully bid to acquire all of the bankrupt brand’s assets via a court-administered process. (Those assets included the brand’s intellectual property rights (namely, its various global trademark registrations, and decades of archives and product prototypes); the commercial leases for its brick-and-mortar outposts in France – from its Saint Germain flagship to a glitzy boutique in Cannes, among others; and its remaining stock of garments and accessories.)The deal is expected to close by the end of October 2021.'
-                    },
-
-
-                    {
-                        title: 'Happy Go Lucky',
-                        dt: '1632787200000',
-                        updatedAt: 'Tue Sep 28 2021',
-                        country: 'United States',
-                        region: 'North America',
-                        industry: 'apparel',
-                        Year: '2021',
-                        content:
-                            'G-III Apparel Group revealed that it has entered into an agreement to purchase Sonia Rykiel, with plans to accelerate the relaunch of the French fashion brand primarily in Europe, for the fall of 2022, with collections across multiple categories. The transaction, which comes less than two years after brothers Eric and Michael Dayan successfully bid to acquire all of the bankrupt brand’s assets via a court-administered process. (Those assets included the brand’s intellectual property rights (namely, its various global trademark registrations, and decades of archives and product prototypes); the commercial leases for its brick-and-mortar outposts in France – from its Saint Germain flagship to a glitzy boutique in Cannes, among others; and its remaining stock of garments and accessories.)The deal is expected to close by the end of October 2021.'
-                    }
-                ],
+                deals: [],
                 searchQuery: null,
                 searchQueryInput: ''
             }
@@ -96,8 +33,8 @@ const app = Vue.createApp({
         resultQuery() {
             if (this.searchQuery) {
                 return this.deals.filter((deal) => {
-                    return this.searchQuery.toLowerCase().split(' ').every(v => (deal.title.toLowerCase().includes(v)) ||
-                        (deal.content.toLowerCase().includes(v)))
+                    return this.searchQuery.toLowerCase().split(' ').every(v => (deal.title.S.toLowerCase().includes(v)) ||
+                        (deal.content.S.toLowerCase().includes(v)))
                 })
             } else {
                 return this.deals;
@@ -105,7 +42,15 @@ const app = Vue.createApp({
         }
     },
 
-})
+    mounted() {
+        fetch('./data/dealblog.json')
+            .then(response => response.json())
+            .then(json => {
+                this.deals = json;
+            })
 
+    }
+
+})
 
 app.mount('#app')
--- component.js
+++ component.js
@@ -1,16 +1,16 @@
-const titleBlocks = Vue.defineComponent({
-  name: 'titleBlock',
+const dealBlock = Vue.defineComponent({
+  name: 'dealBlocks',
   props: ['title', 'country', 'content'],
   template: `
       
       <div id="deal">
      
-      <h2 style="font-family:'Fredoka One';"> {{$props.title}}</h2>
-      <p style="color: orange;"><b>Country</b> {{$props.country}}</p>
-      <p>{{$props.content}}</p>
+      <h2 style="font-family:'ObjectSans-Regular'; font-size: 2rem;"> {{$props.title.S}}</h2>
+      <p style="color: orange;"><b>Country</b> {{$props.country.S}}</p>
+      <p>{{$props.content.S}}</p>
      
       </div>
     `
 })
 
-export default titleBlocks;
+export default dealBlock;
