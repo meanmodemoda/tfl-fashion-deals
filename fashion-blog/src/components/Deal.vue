c0defce9e12d19314f12b919213445b356d19c36
Yang Zhao <freda.y.zhao@gmail.com>
2022-03-14 10:17:14 -0400

    update

---
+++ fashion-blog/src/components/Deal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="deal">
+    <h2>{{ $props.title.S }}</h2>
+    <p class="country"><b>Country</b> {{ $props.country.S }}</p>
+    <p>{{ $props.content.S }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dealBlock",
+  props: ["title", "country", "content"],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.deal h2 {
+  font-family: "ObjectSans-Regular";
+  font-size: 2rem;
+}
+
+.deal .country p {
+  color: orange;
+}
+</style>
